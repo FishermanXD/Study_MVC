c238e065e3fa37b22c29a35e055c7b10c68b11c0
FishermanXD <eltonpescadorvieira@gmail.com>
2018-12-11 20:30:52 -0200

    Primeira importação

---
+++ src/java/br/com/model/User.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.model;
+
+/**
+ *
+ * @author fisherman
+ */
+public class User {
+
+    String nome;
+    String email;
+    String senha;
+    long fone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public long getFone() {
+        return fone;
+    }
+
+    public void setFone(long fone) {
+        this.fone = fone;
+    }
+
+}
---
+++ src/java/br/com/sql/ConectionFactory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author fisherman
+ */
+public class ConectionFactory {
+    
+        public static Connection getConnection() {
+        try{            
+             Class.forName("org.postgresql.Driver");
+                                                   //local do banco                              //usuario  //senha
+             return   DriverManager.getConnection("jdbc:postgresql://localhost:5433/projeto", "postgres", "postgres"); //retorna um Connection
+            
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
